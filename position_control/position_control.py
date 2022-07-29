82ea22f714a624c4db0b163cd77c8171bf6c7ebf
Dieter Buechler <dieter1buechler@gmail.com>
2022-07-29 13:59:20 +0200

    improving position controller

--- position_control/position_control.py
+++ position_control/position_control.py
@@ -4,9 +4,11 @@ import o80
 import pam_interface
 import o80_pam
 import pam_mujoco
+import random
+import numpy as np
 
 
-real_robot = False
+real_robot = True
 
 
 if not real_robot:
@@ -48,14 +50,14 @@ starting_iteration = frontend.latest().get_iteration()
     
 # configuring the controller
 
-kp = [0.2,0.2,0.2,0.2]
-kd = [0.02,0.02,0.02,0.02]
-ki = [0.05,0.05,0.05,0.05]
-ndp = [0.5,0.5,0.6,0.5]
+kp = [0.05,0.5,0.3,0.01]
+kd = [0.0,0.0,0.0,0.0]
+ki = [0.0275,0.055,0.04,0.05]
+ndp = [.9]*4#[0.5,0.6,0.5,0.5]
 time_step = 0.05
 o80_time_step = o80.Duration_us.milliseconds(int(time_step*1000))
-extra_steps = 20
-dq_desired = [0.3]*4
+extra_steps = 25
+dq_desired = [2]*4#[0.6, 0.5, 0.5, 0.5]
 position_controller_factory = o80_pam.position_control.PositionControllerFactory(
     dq_desired,
     robot_config,
@@ -64,79 +66,112 @@ position_controller_factory = o80_pam.position_control.PositionControllerFactory
 )
 
 # going to desired position using the controller
-def go_to(target_position):
+def go_to(target_position,p):
 
-    # starting position
-    current_position = frontend.latest().get_positions()
+    def control():
+        # starting position
+        current_position = frontend.latest().get_positions()
 
-    # constructing the controller
-    controller = position_controller_factory.get(current_position,target_position)
+        # constructing the controller
+        controller = position_controller_factory.get(current_position,target_position)
 
-    # for managing the frequency
-    frequency = o80.FrequencyManager(1./time_step)
+        # for managing the frequency
+        frequency = o80.FrequencyManager(1./time_step)
 
-    # running the controller
-    while controller.has_next():
+        t = time.time()
+        # running the controller
+        while controller.has_next():
 
-        # current state
-        obs = frontend.latest()
-        positions = obs.get_positions()
-        velocities = obs.get_velocities()
+            # current state
+            obs = frontend.latest()
+            positions = obs.get_positions()
+            velocities = obs.get_velocities()
 
-        # getting suitable pressures from the controller
-        pressures = controller.next(positions,velocities)
+            # getting suitable pressures from the controller
+            pressures = controller.next(positions,velocities)
 
-        # applying the pressure
-        for dof, (p_ago,p_antago) in enumerate(pressures):
-            frontend.add_command(dof,p_ago,p_antago,o80_time_step,o80.Mode.OVERWRITE)
-        frontend.pulse()
+            # applying the pressure
+            for dof, (p_ago,p_antago) in enumerate(pressures):
+                frontend.add_command(dof,p_ago,p_antago,o80_time_step,o80.Mode.OVERWRITE)
+            frontend.pulse()
 
-        # making sure we run at the controller
-        # suitable frequency
-        frequency.wait()
+            # making sure we run at the controller
+            # suitable frequency
+            frequency.wait()
+        
+        # evaluating results
+        position = frontend.latest().get_positions()
+        current_it = frontend.latest().get_iteration()
+        current_obs = frontend.get_observations_since(current_it-50)
+        velocities = [[c_obs.get_velocities()[dim] for c_obs in current_obs] for dim in range(4)]
+        error = [abs(p-t) for p,t in zip(position,target_position)]
+        if p:
+            # evaluate if tracking sufficient
+            # print(np.asarray(velocities).T)
+            # print(max(abs(np.mean(velocities,axis=1))))
+            print("mean vels: ",np.mean(velocities,axis=1))
+            print("elapsed time: ",time.time() - t)
+            # print()
+            # print([i/math.pi*180 for i in target_position])
+            # print([i/math.pi*180 for i in position])
+            print([i/math.pi*180 for i in error])
 
-    # evaluating results
-    position = frontend.latest().get_positions()
-    error = [abs(p-t) for p,t in zip(position,target_position)]
-    print()
-    print(target_position)
-    print(position)
-    print(error)
-    print()
+        return position, velocities, error
+    
+    
+    
+    position, velocities, error = control()
+    its=0
+    while ( max([abs(i/math.pi*180) for i in error]) > 10 or max(abs(np.mean(velocities,axis=1))) >1.0 ) and p:
+        if its<10:
+            for dof in range(4):
+                frontend.add_command(dof,25000,25000,o80.Duration_us.seconds(1),o80.Mode.OVERWRITE)
+            frontend.pulse_and_wait()
+            print("   another time ... woohoo: ",its)
+            print("   errs: ",[abs(i/math.pi*180) for i in error])
+            print("   vels: ",abs(np.mean(velocities,axis=1)))
+            control()
+            its +=1
+        
+
+    
+
+    
     
         
 
 # desired positions
+pi2 = math.pi/2.0
 pi4 = math.pi/4.0
 pi6 = math.pi/6.0
-target_position1 = [+pi4,+pi6,-pi6,+pi4]
-target_position2 = [-pi4,-pi6,+pi6,+pi6]
+
+target_position2 = [0,+pi4,0,0]
 
 # starting in a vertical position
-for dof in range(4):
-    frontend.add_command(dof,25000,25000,o80.Duration_us.seconds(3),o80.Mode.OVERWRITE)
-frontend.pulse_and_wait()
+
     
-
-# calling twice in a row for each target position
-go_to(target_position1)
-go_to(target_position1)
-
-go_to(target_position2)
-go_to(target_position2)
-
-#go_to(target_position1)
-#go_to(target_position1)
+for i in range(10):
+    # calling twice in a row for each target position
+    target_position1 = [random.uniform(-pi2, pi2), random.uniform(-pi2, pi2),
+                    random.uniform(-pi2, pi2), random.uniform(-pi2, pi2)]
+    print("---------")
+    print("it ",i)
+    time.sleep(2)
+    go_to(target_position1,False)
+    time.sleep(1)
+    go_to(target_position2,True)
 
 
 # plotting motions of joints
-observations = frontend.get_observations_since(starting_iteration)
-dofs = [[obs.get_positions()[dim] for obs in observations] for dim in range(4)]
-x = [i for i in range(len(observations))]
+# observations = frontend.get_observations_since(starting_iteration)
+# dofs = [[obs.get_positions()[dim]/math.pi*180 for obs in observations] for dim in range(4)]
+# x = [i for i in range(len(observations))]
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-for dim,history in enumerate(dofs):
-    plt.scatter(x,dofs[dim])
-
-plt.show()
+# for dim,history in enumerate(dofs):
+#     plt.scatter(x,dofs[dim])
+# plt.grid()
+# plt.xlabel("time")
+# plt.ylabel("joint angle in deg")
+# plt.show()
