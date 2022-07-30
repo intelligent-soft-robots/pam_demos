ab893e946d1aad7f784a6ecf8276997d5e831bab
Dieter Buechler <dieter1buechler@gmail.com>
2022-07-30 16:51:49 +0200

    paras found for dof 1 and 2

--- position_control/position_control.py
+++ position_control/position_control.py
@@ -50,14 +50,14 @@ starting_iteration = frontend.latest().get_iteration()
     
 # configuring the controller
 
-kp = [0.05,0.5,0.3,0.01]
-kd = [0.0,0.0,0.0,0.0]
-ki = [0.0275,0.055,0.04,0.05]
+kp = [0.4309, 1.212, .4, .05]#[0.06,0.6,0.4,0.03] #[0.05,0.5,0.3,0.01]
+kd = [0.04978, 0.1712, 0.0, 0.0]
+ki = [0.05629, 0.08202, .08, .1]#[0.035,0.07,0.06,0.07]#[0.0275,0.055,0.04,0.05]
 ndp = [.9]*4#[0.5,0.6,0.5,0.5]
 time_step = 0.05
 o80_time_step = o80.Duration_us.milliseconds(int(time_step*1000))
-extra_steps = 25
-dq_desired = [2]*4#[0.6, 0.5, 0.5, 0.5]
+extra_steps = 15
+dq_desired = [math.pi*.75]*4#[0.6, 0.5, 0.5, 0.5]
 position_controller_factory = o80_pam.position_control.PositionControllerFactory(
     dq_desired,
     robot_config,
@@ -78,7 +78,7 @@ def go_to(target_position,p):
         # for managing the frequency
         frequency = o80.FrequencyManager(1./time_step)
 
-        t = time.time()
+        
         # running the controller
         while controller.has_next():
 
@@ -110,7 +110,7 @@ def go_to(target_position,p):
             # print(np.asarray(velocities).T)
             # print(max(abs(np.mean(velocities,axis=1))))
             print("mean vels: ",np.mean(velocities,axis=1))
-            print("elapsed time: ",time.time() - t)
+            
             # print()
             # print([i/math.pi*180 for i in target_position])
             # print([i/math.pi*180 for i in position])
@@ -119,7 +119,7 @@ def go_to(target_position,p):
         return position, velocities, error
     
     
-    
+    t = time.time()
     position, velocities, error = control()
     its=0
     while ( max([abs(i/math.pi*180) for i in error]) > 10 or max(abs(np.mean(velocities,axis=1))) >1.0 ) and p:
@@ -127,11 +127,15 @@ def go_to(target_position,p):
             for dof in range(4):
                 frontend.add_command(dof,25000,25000,o80.Duration_us.seconds(1),o80.Mode.OVERWRITE)
             frontend.pulse_and_wait()
-            print("   another time ... woohoo: ",its)
+            print("   ",its+1,"another time ... woohoo: ")
             print("   errs: ",[abs(i/math.pi*180) for i in error])
             print("   vels: ",abs(np.mean(velocities,axis=1)))
-            control()
+            position, velocities, error = control()
             its +=1
+        else:
+            print("STOPPING: could not move to target pos with sufficient accuracy!")
+            break
+    print("elapsed time: ",time.time() - t)
         
 
     
@@ -150,15 +154,17 @@ target_position2 = [0,+pi4,0,0]
 # starting in a vertical position
 
     
-for i in range(10):
+for i in range(20):
     # calling twice in a row for each target position
     target_position1 = [random.uniform(-pi2, pi2), random.uniform(-pi2, pi2),
-                    random.uniform(-pi2, pi2), random.uniform(-pi2, pi2)]
+                        random.uniform(-pi2, pi2), random.uniform(-pi2, pi2)]
+    # target_position2 = [random.uniform(-pi2, pi2), random.uniform(-pi2, pi2),
+    #                     random.uniform(-pi2, pi2), random.uniform(-pi2, pi2)]
     print("---------")
     print("it ",i)
     time.sleep(2)
     go_to(target_position1,False)
-    time.sleep(1)
+    # time.sleep(1)
     go_to(target_position2,True)
 
 
